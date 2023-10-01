d5d38eb7ff2cde9ea32ece639126c71c0dee2b40
Julian Vos <post@julianvos.nl>
2023-10-01 11:45:44 +0200

    fix: fix build file and remove entire build folder before build

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
+rm -rf ./build
 mkdir ./build
-
-rm build/linux-aarch64.tar.gz
-rm build/linux-amd64.tar.gz
-rm build/linux-arm.tar.gz
-rm build/windows-amd64.tar.gz
 
 tar -czf build/linux-aarch64.tar.gz ./linux-aarch64
 tar -czf build/linux-amd64.tar.gz ./linux-amd64
-tar -czf buildlinux-arm.tar.gz ./linux-arm
+tar -czf build/linux-arm.tar.gz ./linux-arm
 tar -czf build/windows-amd64.tar.gz ./windows-amd64
